9991f01921337a96355b452c8afe6b5c0925e6d1
saurabh.dhumal <saurabh.dhumal@oeis-d35.objectedge.com>
2017-06-22 16:13:57 +0530

    Input from excel file

---
+++ Learn_Selenium/src/selenium_practice/GmailLoginUsingXLFile.java
@@ -0,0 +1,15 @@
+package selenium_practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GmailLoginUsingXLFile {
+
+	public static void main(String[] args) 
+	{
+		WebDriver driver = null;
+		driver = new FirefoxDriver();
+		String inputFilePath="";
+	}
+
+}

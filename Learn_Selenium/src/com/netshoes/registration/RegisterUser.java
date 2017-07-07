271caa4c882294f0922d16e835080f9179115f8f
saurabh.dhumal <saurabh.dhumal@oeis-d35.objectedge.com>
2017-07-07 16:26:48 +0530

    NS Register user small fix

--- Learn_Selenium/src/com/netshoes/registration/RegisterUser.java
+++ Learn_Selenium/src/com/netshoes/registration/RegisterUser.java
@@ -7,14 +7,14 @@ import java.io.IOException;
 import java.util.Iterator;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.formula.functions.Column;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 
 public class RegisterUser 

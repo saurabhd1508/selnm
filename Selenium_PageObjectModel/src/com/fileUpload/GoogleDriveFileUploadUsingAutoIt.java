d14ce12e346529f675e365e2048cf8e259ee0e92
saurabhd1508 <saurabhd1508@gmail.com>
2018-02-23 17:11:09 +0530

    Small refactor

--- Selenium_PageObjectModel/src/com/fileUpload/GoogleDriveFileUploadUsingAutoIt.java
+++ Selenium_PageObjectModel/src/com/fileUpload/GoogleDriveFileUploadUsingAutoIt.java
@@ -13,8 +13,6 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import autoitx4java.AutoItX;
-
 public class GoogleDriveFileUploadUsingAutoIt 
 {
 	WebDriver driver = null;

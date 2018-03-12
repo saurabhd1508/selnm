153a794593ef6cd7f01f245bbe35addbcdcd1b74
saurabhd1508 <saurabhd1508@gmail.com>
2018-03-12 15:11:57 +0530

    Date Driven test

---
+++ Selenium_PageObjectModel/src/org/pom/TestCaller.java
@@ -0,0 +1,18 @@
+package org.pom;
+
+import org.testng.annotations.Test;
+
+public class TestCaller {
+  @Test
+  public void TestMethod1() 
+  {	
+	  System.out.println("Test method 1");
+  }
+  
+  @Test
+  public void TestMethod2() 
+  {	
+	  System.out.println("Test method 2");
+  }
+  
+}
